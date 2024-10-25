159114a22e3077afad902459347fc44fc7c3cd9c
TylerHIP <scriptstesting062@gmail.com>
2024-10-26 01:32:01 +0800

    Update AutoHop.lua

--- AutoHop.lua
+++ AutoHop.lua
@@ -1,15 +1,16 @@
 -- The game ID to hop to
-local gameId = 142823291
+local gameId = 142823291 -- Replace with the actual game ID you want to hop to
 
 -- Function to freeze the game for a specified duration
 local function freezeGame(duration)
-    -- You can just use wait here since freezing the game using character removal is not ideal
+    -- Just a wait function since actually freezing the game is not recommended
     wait(duration)
 end
 
 -- Function to hop to a new server
 local function hopToServer()
-    freezeGame(2) -- Freeze the game for 2 seconds
+    freezeGame(2) -- Freeze the game for 2 seconds before hopping
+    wait(8) -- Wait for an additional 8 seconds (total of 10 seconds) before hopping
     game:GetService("TeleportService"):Teleport(gameId) -- Teleport to the specified game ID
 end
 
